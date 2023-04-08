eb800d1a84f5474fd52f70b39ff7021724cad4ec
mis3rably <mis3rably@gmail.com>
2023-04-08 17:05:00 +0200

    правки после допуска

--- js/data.js
+++ js/data.js
@@ -1,20 +1,3 @@
-const messageList = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
-
-const authorList = [
-  'Неопознанный Кекс',
-  'Артём',
-  'Мария',
-  'Виктор',
-  'Дарья'
-];
-
 const ID_MIN_VALUE = 0;
 const ID_MAX_VALUE = 26;
 const URL_ID_MIN_VALUE = 0;
@@ -26,31 +9,48 @@ const LIKES_MAX = 200;
 const COMMENTS_AMOUNT = 8;
 const DESCRIPTION_AMOUNT = 25;
 
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const authors = [
+  'Неопознанный Кекс',
+  'Артём',
+  'Мария',
+  'Виктор',
+  'Дарья'
+];
+
 function getUniqueRangeInt(min = 0, max) {
-  const prevID = [];
+  const existingNumbers = [];
   return function() {
-    if (prevID.length >= max - 1) {
+    if (existingNumbers.length >= max - 1) {
       return null;
     }
     let result = min;
     result++;
-    while (prevID.includes(result)) {
+    while (existingNumbers.includes(result)) {
       result++;
     }
-    prevID.push(result);
+    existingNumbers.push(result);
     return result;
   };
 }
 
 const generateUniqueNumbersArr = (max, arrLength) => {
-  const arr = [];
-  while(arr.length < arrLength){
+  const uniqueNumbers = [];
+  while(uniqueNumbers.length < arrLength){
     const r = Math.floor(Math.random() * max) + 1;
-    if(arr.indexOf(r) === -1) {
-      arr.push(r);
+    if(uniqueNumbers.indexOf(r) === -1) {
+      uniqueNumbers.push(r);
     }
   }
-  return arr;
+  return uniqueNumbers;
 };
 
 function getRandomInt(min, max) {
@@ -66,8 +66,8 @@ const getCommentID = getUniqueRangeInt();
 const generateComments = () => ({
   id: getCommentID(),
   avatar: `../img/avatar-${getRandomInt(AVATAR_MIN_RANGE, AVATAR_MAX_RANGE)}.svg`,
-  message: messageList[getRandomInt(0, messageList.length - 1)],
-  name: authorList[getRandomInt(0, messageList.length - 1)],
+  message: messages[getRandomInt(0, messages.length - 1)],
+  name: authors[getRandomInt(0, messages.length - 1)],
 });
 
 const generateDescription = () => ({
